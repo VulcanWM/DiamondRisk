5bec743b939a0f8a06474260e50de7da279aef86
Medea <logmedha09@gmail.com>
2022-07-26 13:17:34 +0000

    added functions for 1 and 5 diamonds

--- script.js
+++ script.js
@@ -15,6 +15,65 @@ for (i = 0; i < coll.length; i++) {
 
 var diamonds = 0;
 var health = 50;
-document.getElementById("diamonds").innerText = "Diamonds: " + diamonds
-document.getElementById("health").innerText = "Health: " + diamonds
 
+function endgame(){
+  if (health < 1) {
+    diamonds = 0;
+    health = 50;
+    document.getElementById("diamonds").innerText = "Diamonds: 0"
+    document.getElementById("health").innerText = "Health: 50"
+    return true
+  } else {
+    return false
+  }
+}
+
+function onediamond(){
+  let number = Math.floor(Math.random() * 100) + 1;
+  if (number == 1) {
+    diamonds = 0;
+    health = 50;
+    document.getElementById("diamonds").innerText = "Diamonds: 0"
+    document.getElementById("health").innerText = "Health: 50"
+    document.getElementById("text").innerText = "You got caught by the police."
+  } else if (number == 2){
+    let shoot = Math.floor(Math.random() * 50) + 1;
+    health = health - shoot;
+    let ifendgame = endgame()
+    if (ifendgame == true){
+      document.getElementById("text").innerText = "The police shot and you died because you didn't have enough health!"
+    } else {
+      document.getElementById("text").innerText = "The police shot you and you lost " + shoot + " health!"
+      document.getElementById("health").innerText = "Health: " + health
+    }
+  } else {
+    diamonds = diamonds + 1
+    document.getElementById("diamonds").innerText = "Diamonds: " + diamonds
+    document.getElementById("text").innerText = "You successfully caught 1 diamond!"
+  }
+}
+
+function fivediamond(){
+  let number = Math.floor(Math.random() * 20) + 1;
+  if (number == 1) {
+    diamonds = 0;
+    health = 50;
+    document.getElementById("diamonds").innerText = "Diamonds: 0"
+    document.getElementById("health").innerText = "Health: 50"
+    document.getElementById("text").innerText = "You got caught by the police."
+  } else if (number == 2){
+    let shoot = Math.floor(Math.random() * 50) + 1;
+    health = health - shoot;
+    let ifendgame = endgame()
+    if (ifendgame == true){
+      document.getElementById("text").innerText = "The police shot and you died because you didn't have enough health!"
+    } else {
+      document.getElementById("text").innerText = "The police shot you and you lost " + shoot + " health!"
+      document.getElementById("health").innerText = "Health: " + health
+    }
+  } else {
+    diamonds = diamonds + 5
+    document.getElementById("diamonds").innerText = "Diamonds: " + diamonds
+    document.getElementById("text").innerText = "You successfully caught 5 diamonds!"
+  }
+}
